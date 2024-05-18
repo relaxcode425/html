c87f193ff0f28ba395d77ac01fc8a35297cc5e0c
relaxcode425 <jamesvergaracoronado@gmail.com>
2024-05-17 22:45:21 -0400

    ESQUIZOFRENIA3

--- DLF/validasesion.js
+++ DLF/validasesion.js
@@ -12,17 +12,13 @@ $.validator.setDefaults({
             },
             pass: {
                 required: true,
-                minlength: 8,
-                contieneMayuscula: true,
-                contieneNumero: true
+                minlength: 6
             }
         },
         messages: {           
             pass: {
                 required: "Ingresa tu contraseña",
-                minlength: "Tu contraseña debe tener minimo 8 caracteres",
-                contieneMayuscula: "La contraseña debe contener al menos una letra mayúscula",
-                contieneNumero: "La contraseña debe contener al menos un número"
+                minlength: "Tu contraseña debe tener minimo 8 caracteres"
             },
             usuario: {
                 required: "Por favor ingresa un usuario válido"
@@ -42,18 +38,10 @@ $.validator.setDefaults({
         },
         unhighlight: function (element, errorClass, validClass) {
             $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");  
-        },
-        addMethods: function() {
-            jQuery.validator.addMethod("contieneMayuscula", (value, element) => {
-                return /[A-Z]/.test(value); // Verifica si hay al menos una letra mayúscula
-            }, "La contraseña debe contener al menos una letra mayúscula");
-            jQuery.validator.addMethod("contieneNumero", (value, element) => {
-                return /\d/.test(value); // Verifica si hay al menos un número
-            }, "La contraseña debe contener al menos un número");
         }
     });
 
-    $('#registro').validate({
+    /* $('#registro').validate({
         rules: {
             usuario: {
                 required: true
@@ -122,7 +110,7 @@ $.validator.setDefaults({
                 return /\d/.test(value); // Verifica si hay al menos un número
             }, "La contraseña debe contener al menos un número");
         }
-    });
+    }); */
 
  });
  
